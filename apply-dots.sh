dd7d71c703566c4dc1ed2d8b678a999769539f73
nwugat <nwugat9@gmail.com>
2026-08-21 03:30:49 -0300

    define alacritty config variant for gnome and kde

--- apply-dots.sh
+++ apply-dots.sh
@@ -2,4 +2,15 @@
 
 cd "$(dirname "$0")"
 
+case "$XDG_CURRENT_DESKTOP" in
+    GNOME)
+      ln -sf -T "$(realpath "dot-config/alacritty/gnome.toml"  )"  "dot-config/alacritty/alacritty.toml"
+        ;;
+    KDE)
+      ln -sf -T "$(realpath "dot-config/alacritty/kde.toml"  )"  "dot-config/alacritty/alacritty.toml"
+        ;;
+    *)
+        ;;
+esac
+
 stow --verbose --dotfiles --target "$HOME" .
