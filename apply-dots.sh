3781c726ca9769ba851526a674d54a0f9436ee5d
nwugat <>
2026-09-16 15:58:07 -0300

    add --adopt option

--- apply-dots.sh
+++ apply-dots.sh
@@ -2,6 +2,32 @@
 
 cd "$(dirname "$0")"
 
+flag_adopt=""
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -v|--verbose)
+          # be verbose
+          ;;
+        -h|--help)
+          # print help
+          ;;
+        --adopt)
+          flag_adopt="--adopt"
+          ;;
+        -*)
+            echo "Unknown option: '$1'" >&2
+            exit 1
+            ;;
+        *)
+            echo "Invalid argument: '$1'" >&2
+            exit 1
+            ;;
+    esac
+    shift # remove parsed option
+done
+
+
 case "$XDG_CURRENT_DESKTOP" in
     GNOME)
       ln -sf -T "$(realpath "dot-config/alacritty/gnome.toml"  )"  "dot-config/alacritty/alacritty.toml"
@@ -13,4 +39,4 @@ case "$XDG_CURRENT_DESKTOP" in
         ;;
 esac
 
-stow --verbose --dotfiles --target "$HOME" .
+stow --verbose --dotfiles "$flag_adopt" --target "$HOME" .
