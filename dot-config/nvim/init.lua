0d120ee3bc3229a08df7ccdb0eadee04264b412e
nwugat <nwugat9@gmail.com>
2026-09-16 16:07:10 -0300

    use separate colours file

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -3,10 +3,9 @@ require('plug') --plugin defs
 require('config.opts')
 require('config.keymaps')
 require('config.autocmds')
-require('config.lsp') --lsp configs
+require('config.lsp')
+require('colours')
 --use-case specific
 require('modules.godot')
 require('modules.pkm')
 require('modules.sessions')
---theme
-vim.cmd [[colorscheme catppuccin-mocha]]
---
+++ dot-config/nvim/lua/colours.lua
@@ -0,0 +1,24 @@
+vim.pack.add({
+  -- { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
+  -- { src = 'https://github.com/rose-pine/neovim', name = 'rose-pine' },
+  -- { src = 'https://github.com/rebelot/kanagawa.nvim', name = 'kanagawa' },
+  { src = 'https://github.com/rezniqov/soviet.nvim', name = 'soviet' },
+})
+
+-- catppuccin-latte (light)
+-- catppuccin-frappe
+-- catppuccin-mocha (darkest)
+-- catppuccin-macchiato
+
+-- rose-pine-dawn (light)
+-- rose-pine-main (darkest)
+-- rose-pine-moon
+
+-- kanagawa-wave (cold)
+-- kanagawa-lotus (light)
+-- kanagawa-dragon (warm)
+
+-- soviet-dark
+-- soviet-light
+
+vim.cmd [[colorscheme soviet-dark ]]
